e5d4adcb89d6673164c5c79d6b6d734279f68539
khawarhere <khawarhere@gmail.com>
2021-03-03 04:10:31 +0500

    elb output

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,6 @@
 # Output variable definitions
 
-# output "vpc_name" {
-#   description = "ID of the VPC"
-#   value       = module.vpc.name
-# }
-
-# output "icap_eip_bastion" {
-#   description = "Elastic IP address of the EC2 instance"
-#   value       = aws_eip.icap_eip_bastion.public_ip
-# }
+output "icap_elb_ew1b" {
+  description = "ELB of Icap"
+  value       = module.elb.icap_elb_ew1b
+}
