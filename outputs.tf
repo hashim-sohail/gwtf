e5fd749978d9d5243e18c070a4e09b0e62c70ed0
khawarhere <khawarhere@gmail.com>
2021-03-03 04:13:43 +0500

    output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,16 @@
 # Output variable definitions
 
+output "icap_node1" {
+  description = "Icap ec2 node 1"
+  value       = module.ec2.ec_instances[0].public_ip
+}
+
+output "icap_node2" {
+  description = "Icap ec2 node 2"
+  value       = module.ec2.ec_instances[1].public_ip
+}
+
 output "icap_elb_ew1b" {
   description = "ELB of Icap"
-  value       = module.elb.icap_elb_ew1b
+  value       = module.elb.icap_elb_ew1b.dns_name
 }
